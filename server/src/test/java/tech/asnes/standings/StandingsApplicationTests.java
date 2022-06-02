50ed6a48426115dcc0ecb47d59cff6d9a890e488
nicasnes <31024845+nicasnes@users.noreply.github.com>
2022-06-02 00:30:11 -0400

    Create simple testing infrastructure

--- server/src/test/java/tech/asnes/standings/StandingsApplicationTests.java
+++ server/src/test/java/tech/asnes/standings/StandingsApplicationTests.java
@@ -5,9 +5,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest
-public class StandingsApplicationTests {
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class StandingsApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+	
 	@Autowired
 	private StandingsApplication controller;
 
@@ -16,4 +26,23 @@ public class StandingsApplicationTests {
 		assertThat(controller).isNotNull();
 	}
 
+
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test",
+				String.class)).contains("Hello world!");
+	}
+
+  @Test
+	public void greetingWithNameReturnsHelloName() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/test?name=Nic",
+				String.class)).contains("Hello Nic!");
+	}
+
+  @Test
+	public void databaseConnectionAndSimpleQuery() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/count",
+				String.class)).contains("4");
+	}
 }
